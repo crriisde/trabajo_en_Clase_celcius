4113604aaa30d91c8f901c5760cad67f1755043d
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-09-24 20:19:34 -0600

    Add files via upload

---
+++ celcius_salas/celcius_salas.py
@@ -0,0 +1,13 @@
+print (" ") #esta variable muestra las comillas para el texto
+print ("Salas De La O Cristian David : Mi practica de clase string.py") #esta variable mostrara en la pantalla los datos 
+#de la practica
+print (" ") #esta variable muestra las comillas para el texto 
+
+def farenheit(a): #esta linea sirve para definir los grados farenheit
+    return (a) #esta linea sirve para concluir la defenicion
+a = int(input("Ingresa los grados farenheit: ")) #esta linea sirve para solicitar los grados farenheit
+resultado = (a-32 * (5/9)) #esta linea sirve para definir el resultado
+print ("el resultado de la converccion a celcius es: ") #esta linea sirve para mostrar el resultado 
+print (resultado) #esta linea sirve para mostrar el resultado
+
+print (" ") #esta variable muestra las comillas para el texto 
